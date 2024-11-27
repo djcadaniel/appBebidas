6a037e694a2e1d168e9c07759b9101d03c7448cb
djcadaniel <djcadaniel@gmail.com>
2024-11-27 15:51:27 -0500

    add showing drinks

---
+++ src/stores/recipeSlice.ts
@@ -0,0 +1,32 @@
+import { StateCreator } from "zustand"
+import { getCategories, getRecipes } from "../services/RecipeService"
+import { Categories, Drinks, SearchFilter } from "../types"
+
+
+export type RecipesSliceType = {
+  categories: Categories
+  drinks: Drinks
+  fetchCategories: ()=> Promise<void>
+  searchRecipes: (searchFilters: SearchFilter)=>Promise<void>
+}
+
+export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
+  categories: {
+    drinks: []
+  },
+  drinks: {
+    drinks: []
+  },
+  fetchCategories: async()=>{
+    const categories = await getCategories()
+    set({
+      categories: categories
+    })
+  },
+  searchRecipes: async (filters)=>{
+    const drinks = await getRecipes(filters)
+    set({
+      drinks
+    })
+  }
+})
