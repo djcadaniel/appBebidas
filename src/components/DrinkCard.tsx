8f5f286d4eb0d7ae13307aa98ec4d9aa8faa09fa
djcadaniel <djcadaniel@gmail.com>
2024-11-29 15:58:57 -0500

    add app to delete recipes

--- src/components/DrinkCard.tsx
+++ src/components/DrinkCard.tsx
@@ -1,3 +1,4 @@
+import { useAppStore } from "../stores/useAppStore"
 import { Drink } from "../types"
 
 type DrinkCardProps = {
@@ -6,9 +7,28 @@ type DrinkCardProps = {
 
 
 export const DrinkCard = ({drink}: DrinkCardProps) => {
+
+  const selectRecipe = useAppStore((state) => state.selectRecipe)
+
   return (
-    <div>
-      <h2>{drink.strDrink}</h2>
+    <div className="border shadow-lg">
+      <div className="overflow-hidden">
+        <img 
+          src={drink.strDrinkThumb} 
+          alt={`Imagen de ${drink.strDrink}`}
+          className="hover:scale-125 transition-transform hover:rotate-2"
+        />
+      </div>
+      <div className="p-5">
+        <h2 className="text-2xl truncate font-black">{drink.strDrink}</h2>
+        <button
+          type="button"
+          className="bg-orange-400 hover:bg-orange-500 mt-5 w-full font-bold text-white text-lg"
+          onClick={()=>selectRecipe(drink.idDrink)}
+        >
+          Ver receta
+        </button>
+      </div>
     </div>
   )
 }
--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,22 +1,29 @@
 import { StateCreator } from "zustand"
-import { getCategories, getRecipes } from "../services/RecipeService"
-import { Categories, Drinks, SearchFilter } from "../types"
+import { getCategories, getRecipeById, getRecipes } from "../services/RecipeService"
+import { Categories, Drink, Drinks, Recipe, SearchFilter } from "../types"
+import { FavoritesSliceType } from "./favoritesSlice"
 
 
 export type RecipesSliceType = {
   categories: Categories
   drinks: Drinks
+  selectedRecipe: Recipe
+  modal: boolean
   fetchCategories: ()=> Promise<void>
   searchRecipes: (searchFilters: SearchFilter)=>Promise<void>
+  selectRecipe: (id: Drink['idDrink'])=> Promise<void>
+  closeModal: ()=>void
 }
 
-export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
+export const createRecipesSlice: StateCreator<RecipesSliceType & FavoritesSliceType, [], [], RecipesSliceType> = (set) => ({
   categories: {
     drinks: []
   },
   drinks: {
     drinks: []
   },
+  selectedRecipe: {} as Recipe,
+  modal: false,
   fetchCategories: async()=>{
     const categories = await getCategories()
     set({
@@ -27,6 +34,19 @@ export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
     const drinks = await getRecipes(filters)
     set({
       drinks
+    })
+  },
+  selectRecipe: async(id)=> {
+    const selectedRecipe = await getRecipeById(id)
+    set({
+      selectedRecipe,
+      modal: true
+    })
+  },
+  closeModal: ()=>{
+    set({
+      modal: false,
+      selectedRecipe: {} as Recipe
     })
   }
 })
